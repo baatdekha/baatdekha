46e7eee11ec0d3ac001ab867b2081ea10d701ce9
Kanha Padhan <padhanbenu96@gmail.com>
2026-01-22 10:41:47 +0530

    fix: fixed new page getting auto scrolled
    - added ScrollToTop to return to top.

--- src/Layout/MainLayout.tsx
+++ src/Layout/MainLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import { ScrollToTop } from "./ScrollToTop";
 
 export function MainLayout() {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
       <Header/>
       <main>
         <Outlet/>
